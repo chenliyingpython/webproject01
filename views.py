65f98530445f90e0aa261f600d711786be4d310f
chenliying <710772819@qq.com>
2018-06-26 22:47:18 +0800

    完成了f5的功能

--- views.py
+++ views.py
@@ -2,7 +2,10 @@ def f1(request):
 	print('f1...')
 def f2(request):
         print('f2...')
-eet):
+def f3(request):
         print('f3...')
- f4(request):
+def f4(request):
         print('f4...')
+f5 (request):
+        print('f5...')
+~                       
